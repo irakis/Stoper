12348ea22d6c97df6438af922f2814776f96fc59
Ireneusz Wyszomirski <ireneuszwyszomirski44@gmail.com>
2022-05-25 17:04:52 +0200

    stop button not working

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -4,33 +4,46 @@ import styles from '../components/Clock.module.css';
 const Clock = (props) => {
 
     const [time, setTime] = useState(0);
+    const [stateIntervalId, setIntervalId] = useState(0);
 
-    const start = () => {
-        setInterval((time) => {
-            setTime(prevValue => prevValue + 1);
+    const timer = () => {
+        setInterval(() => {
+            setTime(prevTime => prevTime + 1);
         }, 1);
     }
 
     const timeToDisplay = props.action(time);
 
-    const stop =
-        useEffect(() => {
-            return () => {
-                if (time) clearInterval(start);
-            };
-        }, []);
 
-    return (
-        <div className={styles.container}>
-            <p className={styles.displayField}>{timeToDisplay}</p>
+    
 
-            <button className={styles.button} onClick={e => start(time)}>START</button>
+    const stop = () => {
+        if (stateIntervalId) {
+            clearInterval(stateIntervalId);
+            setIntervalId(0);
+            return
+        }
+        const newIntervalId = setInterval(() => {
+            setTime(prevTime => prevTime + 1)
+        }, 1);
+        setIntervalId(newIntervalId);
+    
+    return () => {
+        if (time) clearInterval(timer);
+    },[] 
+};
 
-            <button className={styles.button} onClick={e => stop}>STOP</button >
+return (
+    <div className={styles.container}>
+        <p className={styles.displayField}>{timeToDisplay}</p>
 
-            <button className={styles.button} onClick={e => setTime(time === 0)}> RESET</button >
-        </div >
-    );
+        <button className={styles.button} onClick={timer}>START</button>
+
+        <button className={styles.button} onClick={e => stop}>STOP</button >
+
+        <button className={styles.button} onClick={e => setTime(time === 0)}> RESET</button >
+    </div >
+);
 };
 
 export default Clock;
